b5a9f56785d98a839af41405808b97f0d74b8838
Neksoos <neksoos@gmail.com>
2026-02-16 20:47:27 +0200

    lihv

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited.
-// It is used by Next.js to add types for the Next.js app.
